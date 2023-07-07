359737cc7fa71353d2a8de6d8ac723094ccbcb42
sasmitapanda02 <130721029+sasmitapanda02@users.noreply.github.com>
2023-07-07 16:10:53 +0530

    HelloWorldServlet.java

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class HelloWorldServlet {
+//in tomcat9 javax used
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+
+//in tomcat10 jakarta used
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalTime;
+
+public class HelloWorldServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("Hello Prodevans");
+        out.println(LocalTime.now());
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+        {
+        super.doPost(req, resp);
+    }
 }
+
